36df7d8e6912016643f8467232fe7bfffb503ea9
Anastasia Valova <morugina.anast@mail.ru>
2024-04-01 22:27:44 +0300

    Small Letters

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int TicketPrice = 146889;
-        int MilesAmount = TicketPrice / 20;
+        int ticketPrice = 146889;
+        int milesAmount = ticketPrice / 20;
         System.out.println("Количество миль:");
-        System.out.println(MilesAmount);
+        System.out.println(milesAmount);
     }
 }
